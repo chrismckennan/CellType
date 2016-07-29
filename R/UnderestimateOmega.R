8e766bf967b04382a81a13fb2ed5acbcb518f079
Chris McKennan <cgm29@uchicago.edu>
2016-07-29 18:08:37 -0500

    added paper

--- R/UnderestimateOmega.R
+++ R/UnderestimateOmega.R
@@ -31,6 +31,37 @@ CompareEstimate <- function(X, alpha, Gamma, Sigma) {     #It is assumed X is n 
 }
 
 
+CompareEstimate2 <- function(X, alpha, E, Gamma, Sigma) {     #It is assumed X is n x d where the first column is a vector of 1's
+                                                       #The only difference between this and the function above is that this takes as input E
+  r <- ncol(Gamma)
+  p <- nrow(Gamma)
+  d <- ncol(X)
+  n <- nrow(X)
+  
+  Q <- qr.Q(qr(X), complete=T)
+  R <- qr.R(qr(X))
+  Q1 <- Q[,1:d]
+  Q2 <- Q[,(d+1):n]
+  W <- matrix(rnorm(r*n), nrow=r, ncol=n)
+  W1 <- W %*% Q1     #r x d
+  E1 <- E %*% Q1
+  
+  Y <- Gamma %*% (W + alpha %*% t(X[,2:d])) + E
+  rm(E)
+  out.em <- fa.em( t( Y %*% Q2 ), r=r )
+  Gamma.hat <- out.em$Gamma
+  Sigma.hat <- out.em$Sigma
+  Info <- t(Gamma.hat / Sigma.hat) %*% Gamma.hat / p
+  Rest <- t(t((Gamma) / Sigma.hat) %*% Gamma.hat / p)    #We are interested in the eigenvalues of Rest'Info^{-2}Rest and if they are all strictly less than 1, which assures alpha is UNDERESTIMATED
+  Inter <- t( (Gamma.hat - Gamma) / Sigma.hat ) %*% Gamma.hat / p
+  rm(out.em)
+  
+  alpha.hat <- solve(t(Gamma.hat / Sigma.hat) %*% Gamma.hat, t(Gamma.hat / Sigma.hat) %*% Y %*% Q1 %*% solve(t(R)))[,2:d]
+  alpha.hat.2 <- solve(t(Gamma.hat / Sigma) %*% Gamma.hat, t(Gamma.hat / Sigma) %*% Y %*% Q1 %*% solve(t(R)))[,2:d]
+  alpha.true <- alpha + (W1 %*% solve(t(R)))[,2:d]
+  return(list(alpha.true=alpha.true, alpha.hat=alpha.hat, alpha.hat.2=alpha.hat.2, resid=solve(t(Gamma.hat / Sigma.hat) %*% Gamma.hat, t(Gamma.hat / Sigma.hat) %*% E1 %*% solve(t(R)))[,2:d], Info=Info, Rest=Rest, Inter=Inter, Sigma.hat=Sigma.hat))
+}
+
 ###This code will simulate data and analyze it using cate (without cell type info). The results will be used in the Fdr figure###
 
 Sim.Analyze.Fdr <- function(X, C, B, L, Sigma) {   #It is assumed that d = 2 and the first row of X is the intercept
